dac1afa0e98a530474ef885187454f595851f52f
Berlin <berlinsantoscruz@gmail.com>
2022-10-15 17:20:14 -0400

    pwa added

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.blackshark.github',
   appName: '6502',
   webDir: 'www',
   bundledWebRuntime: false
